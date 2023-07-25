bba92d3cec4a40e69881814deb98936c9ee6372c
Clari Loo <clarizlook.tech@gmail.com>
2023-07-25 22:24:09 +0800

    Added github workflow

--- bin/aws-cdk-constructs.ts
+++ bin/aws-cdk-constructs.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register'
+import * as vars from '../vars'
 import * as cdk from 'aws-cdk-lib'
 import { SqsStack } from '../lib/sqs-stack'
 
@@ -7,7 +8,7 @@ const app = new cdk.App()
 
 new SqsStack(app, 'SqsStack', {
   env: {
-    account: '8',
-    region: 'ap-southeast-2',
+    account: vars.ACCOUNT_ID,
+    region: vars.REGION,
   },
 })
